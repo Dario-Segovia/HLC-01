4a1fef0350d9e194d058f9d966ffda1189455e1d
Dario <dario010904@gmail.com>
2024-10-16 08:26:03 +0200

    primer commit

---
+++ U01/prueba3.php
@@ -0,0 +1,20 @@
+</head>
+<body>
+    <!-- Título de la práctica -->
+    <h1>Práctica 1.3 Aleatoriedad</h1>
+
+    <!-- Párrafo con información del alumno, módulo y año -->
+    <p>Nombre del alumno: Darío Segovia<br>
+       Módulo: Horas de Libre Configuración<br>
+       Año: 2024<br>
+    </p>
+
+    
+   <p>Número aleatorio: <?php echo rand(1, 100); ?></p>
+  
+
+    <!-- Párrafo con la fecha y hora actual del servidor -->
+    <p>Fecha y hora actual del servidor: 
+        <?php echo date("d-m-Y H:i:s"); ?></p>
+</body>
+</html>
